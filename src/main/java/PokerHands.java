380f82df23689ce60a3f74af115b27b1b053c2df
ainemccaughey <a.mccaughey@hotmail.co.uk>
2023-04-01 19:41:50 +0100

    Updating file read to get filepath from console input

--- src/main/java/PokerHands.java
+++ src/main/java/PokerHands.java
@@ -2,22 +2,29 @@ import entities.Hand;
 import entities.HandFactory;
 
 import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class PokerHands {
 
-    public static void main(String[] args) throws IOException {
-        PokerHands pokerHands = new PokerHands();
-        pokerHands.readTestsTxtFile("Hands.txt");
+    public static void main(String[] args) throws FileNotFoundException {
+        System.out.println("Welcome to Poker Face!");
+        System.out.println("To get started, enter the full file path of the file you would like evaluated");
+
+        // Read the filepath from console
+        Scanner scanner = new Scanner(System.in);
+        String filename = scanner.nextLine();
+        readTestsTxtFile(filename);
     }
 
-    private void readTestsTxtFile(String fileName) throws IOException {
-        InputStream inputStream = getClass().getClassLoader().getResourceAsStream(fileName);
-        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-        String hand;
-        while ((hand = reader.readLine()) != null) {
+    static void readTestsTxtFile(String fileName) throws FileNotFoundException {
+        BufferedReader fileBuffer = new BufferedReader(new FileReader(fileName));
+        List<String> lines = fileBuffer.lines().collect(Collectors.toList());
+
+        for(String hand: lines) {
             Hand cards = HandFactory.MakeHandFromString(hand.trim());
             System.out.println(cards + " => " + cards.handName());
         }
